ec3d9a0a49ca5a1bd5ba4f1eca1b9de40b57c9d4
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2022-09-09 16:26:51 +0800

    renoir: Move over to prebuilt AOSP way of bundling kernel
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,17 +24,22 @@ DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/hidl/manifest.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/hidl/framework_compatibility_matrix.xml
 
-# Kernel
-TARGET_KERNEL_CONFIG += vendor/renoir_QGKI.config
-
 # Kernel modules
+TARGET_KERNEL_DIR := device/xiaomi/renoir-kernel
 BOOT_KERNEL_MODULES := \
     focaltech_touch.ko \
     goodix_core.ko \
     hwid.ko \
     msm_drm.ko \
     xiaomi_touch.ko
+
+KERNEL_MODULE_DIR := $(TARGET_KERNEL_DIR)
+KERNEL_MODULES := $(wildcard $(KERNEL_MODULE_DIR)/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
+BOARD_BUILD_VENDOR_RAMDISK_IMAGE := true
+BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD)))
 
 # Partitions
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
