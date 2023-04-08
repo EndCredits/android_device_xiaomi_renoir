ae53dd10d73c64db61b438257b6212ed3647818d
EndCredits <endcredits@crepuscular-aosp.icu>
2023-04-08 23:24:15 +0800

    renoir: Initial bring up for DerpFest
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- derp_renoir.mk
+++ derp_renoir.mk
@@ -11,14 +11,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from renoir device
 $(call inherit-product, device/xiaomi/renoir/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFest stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := renoir
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2101K9C
-PRODUCT_NAME := lineage_renoir
+PRODUCT_NAME := derp_renoir
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_SYSTEM_NAME := renoir
 PRODUCT_SYSTEM_DEVICE := renoir
