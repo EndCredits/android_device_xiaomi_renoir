2b9fa4ce510f00d19ec2b579f49eb2c4fb5558a6
EndCredits <endcredits@crepuscular-aosp.icu>
2023-06-11 12:34:03 +0800

    renoir: Ship display configs in tree
     * This should improve adaptive brightness stability and correct
       brightness mapping.
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- device.mk
+++ device.mk
@@ -16,6 +16,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_shimaidp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/sound_trigger_mixer_paths_shimaidp.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_shima/sound_trigger_platform_info.xml
 
+# Display Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/display/display_id_4630946480857061762.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/display_id_4630946480857061762.xml \
+    $(LOCAL_PATH)/configs/display/display_id_4630946545580055170.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/display_id_4630946545580055170.xml
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
