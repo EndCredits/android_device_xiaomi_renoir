abb2db1e9db22c59f70d136b65aef1f2e2fab97f
EndCredits <endcredits@crepuscular-aosp.icu>
2023-10-12 11:18:47 +0800

    renoir: Switch to prebuilt kernel
     * Also overrided kernel headers defined in the source
    
    Signed-off-by: EndCredits <endcredits@crepuscular-aosp.icu>

--- Android.mk
+++ Android.mk
@@ -8,6 +8,11 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),renoir)
 
+$(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr: $(wildcard $(PRODUCT_VENDOR_KERNEL_HEADERS)/*)
+	rm -rf $@
+	mkdir -p $@/include
+	cp -a $(PRODUCT_VENDOR_KERNEL_HEADERS)/. $@/include
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
